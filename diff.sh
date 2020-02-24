b9ad67cdc70977901665cf5d7157a6e41c9dc2af
Qvasov <Kvasovmi@yandex.ru>
2020-02-24 22:27:32 +0300

    Merge branch 'visu' of https://github.com/Qvasov/corewar into visu
    # Conflicts:
    #	vm/inc/vm.h
    #	vm/src/cw_flag_v4.c
    #	vm/src/ft_battle.c
    #	vm/src/ft_cycle.c
    #	vm/src/ft_op_add_sub_and_or_xor.c
    #	vm/src/ft_op_ld_ldi_lld_lldi.c
    #	vm/src/ft_op_live_zjmp_fork_lfork_aff.c
    #	vm/src/ft_op_st_sti.c
    #	vm/src/ft_valid_op_code_and_reg.c
    #	vm/src/main.c

--- diff.sh
+++ diff.sh
@@ -1,13 +1,7 @@
 rm 0 1
-NUM=6;
+NUM=15;
 ./cmake-build-debug/corewar -o -dump $1 -v $NUM \
-	../aagripin_corewar/testing_suite/testers/newguy.cor \
-	vm_champs/champs/jumper.cor \
-	vm_champs/champs/jumper.cor \
-	vm_champs/champs/jumper.cor > 0
+	vm_champs/xor_d4.cor > 0
 ./vm_champs/corewar -a -d $1 -v $NUM \
-	../aagripin_corewar/testing_suite/testers/newguy.cor \
-	vm_champs/champs/jumper.cor \
-	vm_champs/champs/jumper.cor \
-	vm_champs/champs/jumper.cor > 1
+	vm_champs/xor_d4.cor > 1
 diff 0 1
